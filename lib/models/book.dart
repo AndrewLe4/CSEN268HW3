89ab3a0924d11e08b1811a60ecec6a7f4648a2c6
AndrewLe4 <160354249+AndrewLe4@users.noreply.github.com>
2025-10-26 23:19:03 -0700

    Update book.dart

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,12 +1,16 @@
-//Book class
-class Book {
+import 'package:equatable/equatable.dart';
+
+class Book extends Equatable {
   final String title;
   final String author;
   final String description;
 
-  Book({
+  const Book({
     required this.title,
     required this.author,
     required this.description,
   });
+
+  @override
+  List<Object?> get props => [title, author, description];
 }
